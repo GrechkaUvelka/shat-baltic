dc45c16b99ac92755f04400c75421fbd51f6cd5a
Timofey <nenashev.t@mail.ru>
2018-08-28 02:08:46 +0700

    Heroku

---
+++ main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django import forms
+from django.core.mail import send_mail, BadHeaderError
+class ContactForm(forms.Form):
+    name = forms.CharField()
+
+
+def index(request):
+    return render(request, "index.html")
+
+def send(request):
+    email = request.POST.get('email')
+    s_name = request.POST.get('s_name')
+    name = request.POST.get('name')
+    tel = request.POST.get('tel')
+    date = request.POST.get('date')
+    time = request.POST.get('time')
+    num = request.POST.get('num')
+
+    if s_name and name and tel and email:
+        body = s_name + " " + name + "\nТелефон: " + tel + "\nДата и время " + date + " " + time + "\n" + "Количество человек: " + num + "\nПочта: " + email
+        send_mail('Бронирование', body, email, ["shatbaltic39@gmail.com"], fail_silently=False)
+        return render(request, 'success.html')
+    else:
+        return render(request, "fail.html")
