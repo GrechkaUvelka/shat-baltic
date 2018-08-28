ed06644f02959564b286247e26a1b6a9066896f2
Timofey <nenashev.t@mail.ru>
2018-08-28 15:31:26 +0700

    1

--- restoran/settings.py
+++ restoran/settings.py
@@ -128,5 +128,5 @@ STATICFILES_DIRS = (
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "mmmmaslooo@gmail.com"
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_H_PASSWORD', '0')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_H_PASSWORD', '')
 EMAIL_USE_TLS = True
