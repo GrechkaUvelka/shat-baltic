6c76021d9f50226ad07354a302a7f8b0c7562f4b
Timofey <nenashev.t@mail.ru>
2018-08-28 15:59:47 +0700

    1

--- restoran/settings.py
+++ restoran/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '_dg+x*q6bu*n^$xjngy%%if^)-i%ki#%kmw%pdmwj2g5d3s0!u'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'eu09(ilk6@4sfdofb=b_2ht@vad*$ehh9-)3u_83+y%(+phh&=')
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ["shat-baltic.herokuapp.com", "127.0.0.1"]
